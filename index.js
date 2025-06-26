7facf9876dcf2552423663e71fb85880f209dd1a
PenProse <penproseservices@gmail.com>
2025-06-26 10:19:33 +0500

    Update Cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 
 // middlewares
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://penprose.github.io',
   credentials: true,
   methods: ['POST', 'GET', "DELETE"]
 }))
